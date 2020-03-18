f9a7748670a159ea872739c0b2db889db33959e3
Keiran <hello@keiranoleary.com>
2020-03-18 20:17:30 +1000

    Add @types/aws-lambda

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,4 +1,6 @@
-export const world = async (event: any) => ({
+import { APIGatewayEvent } from "aws-lambda";
+
+export const world = async (event: APIGatewayEvent) => ({
   statusCode: 200,
   body: JSON.stringify({
     message: "Hello world",
